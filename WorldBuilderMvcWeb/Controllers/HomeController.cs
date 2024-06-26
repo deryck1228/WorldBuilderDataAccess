c44bd215e43f99eecc5fb397ab84127b8734f759
Ben Smith <deryck1228@yahoo.com>
2024-06-26 14:35:18 -0700

    Update UpdateCharacter function to exclude trying to update primary key

---
+++ WorldBuilderMvcWeb/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorldBuilderWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Redirect to Character/Index with a default ID or handle appropriately
+            return RedirectToAction("Index", "Character", new { id = 1 });
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
